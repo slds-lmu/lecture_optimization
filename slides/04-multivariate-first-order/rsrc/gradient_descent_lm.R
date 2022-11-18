269293156d964dd76c80e8c16015d1725608bb7c
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2022-11-18 17:14:22 +0100

    BB final edit 4.1 bis 4.3

--- slides/04-multivariate-first-order/rsrc/gradient_descent_lm.R
+++ slides/04-multivariate-first-order/rsrc/gradient_descent_lm.R
@@ -13,7 +13,7 @@ p
 th0 = c(0, 0)
 alpha = 0.1
 
-progress = c(t = 0, theta0 = th0[1], theta1 = th0[2], L = sum((Y - X %*% th)^2))
+progress = c(t = 0, theta0 = th0[1], theta1 = th0[2], R = sum((Y - X %*% th0)^2))
 
 th = th0
 for (t in 1:30) {
@@ -21,13 +21,14 @@ for (t in 1:30) {
 	grad = 2 * colSums(diag(as.vector(Y - X %*% th)) %*% X) 
 	grad = grad / sqrt(sum(grad^2))
 	th = th + alpha * grad
-	L = sum((Y - X %*% th)^2)
+	R = sum((Y - X %*% th)^2)
 
-	progress = rbind(progress, c(t = t, theta0 = th[1], theta1 = th[2], L = sum((Y - X %*% th)^2)))
+	progress = rbind(progress, c(t = t, theta0 = th[1], theta1 = th[2], R = sum((Y - X %*% th)^2)))
+	print(progress)
 }
 
 
-p1 = ggplot(data = as.data.frame(progress), aes(x = t, y = L)) + geom_line() + xlim(c(1, nrow(progress)))
+p1 = ggplot(data = as.data.frame(progress), aes(x = t, y = R)) + geom_line() + xlim(c(1, nrow(progress)))
 p1
 
 p2 = p + geom_abline(data = as.data.frame(progress), aes(intercept = theta0, slope = theta1, colour = t), alpha = 0.5)
