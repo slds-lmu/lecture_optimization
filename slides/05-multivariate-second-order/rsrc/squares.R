05f721f9213ae482b41f3f2646176ab06bffa3f6
Marius Müller <--marius.mueller@gmx.de>
2022-12-02 16:03:17 +0100

    added 5.1, 5.2, 5.3, 5.4 .tex files

---
+++ slides/05-multivariate-second-order/rsrc/squares.R
@@ -0,0 +1,28 @@
+
+
+
+library(knitr)
+library(microbenchmark)
+library(snow)
+library(colorspace)
+library(ggplot2)
+library(zoo)
+library(gridExtra)
+source("rsrc/functions.R")
+
+
+
+x = c(1, 2, 4, 5, 8)
+y = c(3, 5, 6, 13, 20)
+mod = nls(y ~ a*exp(b*x), start = list(a = 1, b = 0.2))
+
+d = data.frame(x = x, y = y, pred = predict(mod))
+
+plot = ggplot(data = d, aes(x = x, y = y)) + geom_point()
+plot = plot + geom_smooth(method = "nls", formula = y ~ a*exp(b*x),method.args = list(start = c(a = 1, b = 0.2)), se = FALSE, color = "black" )
+plot = plot + geom_point(aes(x = x, y = pred))
+plot = plot + geom_segment(aes(x = x, y = y, xend = x, yend = pred), color = "red")
+plot = plot + theme_bw()
+plot
+
+ggsave("figure_man/squares.png", width = 4, height = 3)
