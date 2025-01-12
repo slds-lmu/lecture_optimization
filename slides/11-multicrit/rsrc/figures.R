07526162575b3640638922118abe45c9230cd2a8
ludwigbothmann <46222472+ludwigbothmann@users.noreply.github.com>
2025-01-12 20:06:38 +0100

    iter 3 of new chapter on multicrit

--- slides/11-multicrit/rsrc/figures.R
+++ slides/11-multicrit/rsrc/figures.R
@@ -1,21 +1,24 @@
+### This short script generates exemplary plots ofthe objectives plotted against each other 
+### as well as against the decision varianble x
+
 library(ggplot2)
 
 fun = function(x) (x - 1)^2
 p = ggplot(data.frame(x = c(0, 3)), aes(x)) + stat_function(fun = fun)
 p = p + geom_point(x = 1, y = 0, color = "green", size = 3)
-p = p + theme_bw() + ylab("c") + xlab(expression(lambda))
-ggsave(p, filename = "../images/graph1.png", height = 2, width = 2)
+p = p + theme_bw() + ylab("f") + xlab(expression(x))
+ggsave(p, filename = "../figure_man/graph1.png", height = 2, width = 2)
 
 fun1 = function(x) (x - 1)^2
 fun2 = function(x) 3 * (x - 2)^2
 p = ggplot(data.frame(x = c(0, 3)), aes(x)) + stat_function(fun = fun1) + stat_function(fun = fun2, color = "blue")
-p = p + theme_bw() +  ylab("c") + xlab(expression(lambda))
+p = p + theme_bw() +  ylab("f") + xlab(expression(lambda))
 
-ggsave(p, filename = "../images/graph2.png", height = 2, width = 2)
+ggsave(p, filename = "../figure_man/graph2.png", height = 2, width = 2)
 
 x = seq(0, 3, length.out = 1000)
 xpareto = seq(1, 2, length.out = 1000)
 
-p2 = ggplot() + geom_point(data = data.frame(c1 = fun1(x), c2 = fun2(x)), aes(x = c1, y = c2), size = 0.05) + geom_point(data = data.frame(c1 = fun1(xpareto), c2 = fun2(xpareto)), aes(x = c1, y = c2), color = "green", size = 0.05) + theme_bw()
+p2 = ggplot() + geom_point(data = data.frame(f1 = fun1(x), f2 = fun2(x)), aes(x = f1, y = f2), size = 0.05) + geom_point(data = data.frame(f1 = fun1(xpareto), f2 = fun2(xpareto)), aes(x = f1, y = f2), color = "green", size = 0.05) + theme_bw()
 
-ggsave(p2, filename = "../images/graph3.png", height = 2, width = 2)
+ggsave(p2, filename = "../figure_man/graph3.png", height = 2, width = 2)
