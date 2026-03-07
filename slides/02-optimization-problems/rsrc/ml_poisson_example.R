8cb5a33827d5b51bcfe1ab28367d744ff33f57a6
manuel helmerichs <manuelhelmerichs@icloud.com>
2026-03-07 10:34:21 +0100

    Chapter 02: Delted stale figures

--- slides/02-optimization-problems/rsrc/ml_poisson_example.R
+++ slides/02-optimization-problems/rsrc/ml_poisson_example.R
@@ -16,11 +16,6 @@ load("e100.RData")
 
 e99 = e100[-which.max(e100)] # start without a tricky outlier
 
-p = ggplot() + geom_histogram(data = data.frame(x = factor(e99, levels = c(0, 1, 2), ordered = TRUE)), aes(x = x), stat = "count") + theme_bw()
-p
-ggsave("../figure/ml_poisson_example_1.pdf", p, width = 3, height = 2.5)
-
-
 likelihood = function(lambda) {
   sum(dpois(e99, lambda = lambda, log = TRUE))
 }
@@ -33,10 +28,9 @@ optimum = optimize(likelihood, interval = c(0, 1), maximum = TRUE)
 
 p = ggplot() + geom_line(data = df, aes(x = lambda, y = f)) + geom_point(data = data.frame(x = optimum$maximum, y = optimum$objective), aes(x = x, y = y), colour = "blue") + xlab("x")
 p
-ggsave("../figure/ml_poisson_example_2.pdf", p, width = 3, height = 2)
+ggsave("../figure/ml_poisson_example_1.pdf", p, width = 3, height = 2)
 
 
 p = ggplot() + geom_line(data = df, aes(x = lambda, y = - f)) + geom_point(data = data.frame(x = optimum$maximum, y = - optimum$objective), aes(x = x, y = y), colour = "blue") + xlab("x")
 p
-ggsave("../figure/ml_poisson_example_3.pdf", p, width = 3, height = 2)
-
+ggsave("../figure/ml_poisson_example_2.pdf", p, width = 3, height = 2)
